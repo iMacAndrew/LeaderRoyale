4bdab5721a2701f1a90b18b07d55b3a1aa1c3677
imachumphries <imachumphries@me.com>
2019-01-12 23:49:18 -0500

    Make clan name table cards

--- LeaderRoyale/Clan Name Table/ClanNameTableViewCell.swift
+++ LeaderRoyale/Clan Name Table/ClanNameTableViewCell.swift
@@ -9,14 +9,11 @@
 import UIKit
 
 class ClanNameTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var clanNameLabel: UILabel!
     @IBOutlet weak var memberCountLabel: UILabel!
     @IBOutlet weak var clanLogo: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
+
     func configure(with clanInfo: ClanInfo?) {
         if let clanName = clanInfo?.name {
             clanNameLabel.text = String(clanName)
